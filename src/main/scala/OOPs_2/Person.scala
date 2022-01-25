1dbe91d11189828baa58151cc08f4db5f8abf1c4
Ayush Rai <94011475+Ayush15rai@users.noreply.github.com>
2022-01-26 01:53:34 +0530

    Update and rename person.scala to Person.scala

--- src/main/scala/OOPs_2/Person.scala
+++ src/main/scala/OOPs_2/Person.scala
@@ -1,8 +1,8 @@
 package OOPs_2
 
-class person(var name: String, var age: Int) extends Ordered[person]{
+class Person(var name: String, var age: Int) extends Ordered[Person]{
 
-  override def compare(that: person): Int = {
+  override def compare(that: Person): Int = {
     this.name compare that.name match {
       case 0 => if (this.age.equals(that.age))
         println("True\nName and age both are same")
@@ -21,9 +21,9 @@ class person(var name: String, var age: Int) extends Ordered[person]{
 
 }
 
-object person extends App {
-  var firstPerson = new person("Lucifer", 24)
-  var secondPerson = new person("Amenadiel", 28)
+object Person extends App {
+  var firstPerson = new Person("Lucifer", 24)
+  var secondPerson = new Person("Amenadiel", 28)
   firstPerson compare secondPerson
 }
 
