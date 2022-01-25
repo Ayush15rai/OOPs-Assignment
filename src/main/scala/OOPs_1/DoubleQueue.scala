af60d8cc91ed71d7a6ea99483fdc33453f7b3c3f
Ayush Rai <94011475+Ayush15rai@users.noreply.github.com>
2022-01-26 00:12:43 +0530

    Update DoubleQueue.scala

--- src/main/scala/OOPs_1/DoubleQueue.scala
+++ src/main/scala/OOPs_1/DoubleQueue.scala
@@ -2,7 +2,7 @@ package OOPs_1
 
 import scala.collection.mutable.ListBuffer
 
-class doubleQueue(list: ListBuffer[Int]) extends Queue {
+class DoubleQueue(list: ListBuffer[Int]) extends Queue {
   for ( i <- list.indices){
     list.update(i, 2 * list(i))
   }
